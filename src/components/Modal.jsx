4dbc9d60853944ebe7b20aa2707e2e6a758737d2
Timi <timilehinoladapo0@gmail.com>
2023-10-22 23:13:26 +0100

    feat:modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Modal({setOpenModal }) {
+  return (
+    <section onClick={()=>{setOpenModal(false)}} className='modal_background'>
+        <div className='centered'>
+        <div className='modal flex flex-col gap-8 px-16 py-16 '>
+        <p className='text-center text-lg font-bold'>Congratulations</p>
+        <p className='text-center text-lg font-bold'>You have successfully Registered!</p>
+        <p className='text-center'>Yes, it was easy and you did it! <br/> check your mail box for next step</p>
+        <button onClick={()=>setOpenModal(false)} className="w-full rounded p-3 outline-none text-white bg-gradient-to-r from-purple-600 via-purple-400 to-pink-400 h-3.3rem">Back</button>
+        </div>
+        </div>
+        </section>
+  )
+}
+
+export default Modal
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import {React, useState} from "react";
 import registerBackGround from "../assets/registerBackground.png";
 import Navbar from "../components/Navbar";
 import man from "../assets/man.png";
 import woman from "../assets/woman.png";
+import Modal from "../components/Modal";
 
 function Register() {
+	const [openModal, setOpenModal] = useState(false);
 	const InputGroup = () => {
 		return (
 			<div className='flex flex-col'>
@@ -31,7 +33,7 @@ function Register() {
 					className='border border-white rounded bg-[#ffffff08] p-2.5 mt-2 outline-none'
 				>
 					{options?.map((option) => (
-						<option className='bg-[#ffffff08]' value={option}>
+						<option key={option} className='customSelectOption' value={option}>
 							{option}
 						</option>
 					))}
@@ -123,13 +125,14 @@ function Register() {
 					</p>
 
 					<div className='flex gap-2 items-center'>
-						<input type='checkbox' name='terms' id='terms' required />
+						<input  type='checkbox' name='terms' id='terms' required />
 						<label htmlFor='terms'>
 							I agreed with the event terms and conditions and privacy policy
 						</label>
 					</div>
 
-                    <button className="w-full rounded p-3 outline-none text-white bg-gradient-to-r from-purple-600 via-purple-400 to-pink-400 h-3.3rem">Register Now</button>
+                    <button onClick={()=>setOpenModal(true)} className="w-full rounded p-3 outline-none text-white bg-gradient-to-r from-purple-600 via-purple-400 to-pink-400 h-3.3rem">Register Now</button>
+					{openModal && <Modal setOpenModal={setOpenModal} />}
 				</form>
 			</section>
 		</div>
